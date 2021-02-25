9b2f7adff322988473259cc5423a51569cb36b0f
Kolt <ilya.kolt@gmail.com>
2021-02-25 15:16:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Требуется написать функцию бинарного поиска,
+посчитать его асимптотическую сложность 
+и проверить работоспособность функции. */
+
+namespace Binary_search
+{
+    class Program
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        static void Main()
+        {
+            Test test = new Test();
+            Console.WriteLine("Подаётся массив: 1 6 77 123 444 555 660\nИщется число: 88\nОжидается: -1\nРезультат: " + test.DoMass1_ReturnMinusOne());
+            Console.WriteLine("\nПодаётся массив: 1 6 77 123 444 555 660\nИщется число: 77\nОжидается: 2\nРезультат: " + test.DoMass1_ReturnTwo());
+            Console.WriteLine("\nАссимптотическая сложность алгоритма равна O(N) ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    public class Test
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        //подаётся массив 1 6 77 123 444 555 660
+        //ищется число 88
+        //ожидается -1
+        public int DoMass1_ReturnMinusOne()
+        {
+            int[] intArray = new int[] { 1, 6, 77, 123, 444, 555, 660 };
+            return BinarySearch(intArray, 88);
+        }
+
+        //подаётся массив 1 6 77 123 444 555 660
+        //ищется число 77
+        //ожидается 2
+        public int DoMass1_ReturnTwo()
+        {
+            int[] intArray = new int[] { 1, 6, 77, 123, 444, 555, 660 };
+            return BinarySearch(intArray, 77);
+        }
+        //
+    }
+}
+
